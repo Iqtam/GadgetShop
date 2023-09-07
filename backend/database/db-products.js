61467d6f945ae154ba6a0bddd2ff4b553144dd21
MinhazFuad29 <minhajul.fuad@gmail.com>
2023-09-08 02:12:10 +0600

    create, update product, filter functionality

--- backend/database/db-products.js
+++ backend/database/db-products.js
@@ -122,8 +122,8 @@ async function getProductBYId(id) {
   }
 }
 
-async function searchProducts(searchQuery) {
 
+async function searchProducts(searchQuery) {
   const sql = `
   SELECT *
   FROM PRODUCT P 
@@ -149,6 +149,149 @@ async function searchProducts(searchQuery) {
   }
 }
 
+async function createProduct(productData) {
+  const {PRODUCT_ID, TITLLE, PRICE, STOCK, DESCRIPTION, IMAGE, SUPPLIER_ID, CATEGORY_ID, BRAND} = productData;
+  console.log(productData);
+  const sql = `
+  INSERT INTO PRODUCT (PRODUCT_ID, TITLLE, PRICE, STOCK, DESCRIPTION, IMAGE, SUPPLIER_ID, CATEGORY_ID, BRAND) VALUES (:product_id, :titlle, :price, :stock, :description, :image, :supplier_id, :category_id, :brand)
+  `;
+  console.log(productData);
+
+  const binds = {
+    product_id: PRODUCT_ID,
+    titlle: TITLLE,
+    price: PRICE,
+    stock: STOCK,
+    description: DESCRIPTION,
+    image: IMAGE,
+    supplier_id: SUPPLIER_ID,
+    category_id: CATEGORY_ID,
+    brand: BRAND
+  };
+  try {
+    const result = await database.dbexecute(sql, binds, database.dboptions);
+    if (result) {
+      return result.rows;
+    } else {
+      console.error("Query result is undefined.");
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+}
+
+async function getAllCategories() {
+  const sql = `
+  SELECT *
+  FROM CATEGORY
+  WHERE CHILD_PARENT_CATEGORY_ID IS NOT NULL
+  `;
+  const binds = {};
+  try {
+    const result = await database.dbexecute(sql, binds, database.dboptions);
+    if (result) {
+      return result.rows;
+    } else {
+      console.error("Query result is undefined.");
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+}
+
+async function updateProductById(id, updatedProductData) {
+  const sql = `
+    UPDATE PRODUCT
+    SET TITLLE = :titlle, PRICE = :price, STOCK = :stock, DESCRIPTION = :description, IMAGE = :image,  BRAND = :brand
+    WHERE PRODUCT_ID = :id
+  `;
+
+  const binds = {
+    titlle: updatedProductData.TITLLE,
+    price: updatedProductData.PRICE,
+    stock: updatedProductData.STOCK,
+    description: updatedProductData.DESCRIPTION,
+    image: updatedProductData.IMAGE,
+    brand: updatedProductData.BRAND,
+    id: id,
+  };
+
+  try {
+    const result = await database.dbexecute(sql, binds, database.dboptions);
+    if (result) {
+      return result.rows;
+    } else {
+      console.error('Query result is undefined.');
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+}
+
+async function getAllBrands() {
+  const sql = `
+  SELECT DISTINCT BRAND
+  FROM PRODUCT 
+  `;
+  const binds = {};
+  console.log("Hello");
+  try {
+    const result = await database.dbexecute(sql, binds, database.dboptions);
+    if(result){
+      return result.rows;
+    } else {
+      console.error("Query result is undefined.");
+    }
+  } catch(error) {
+    console.error("An error occurred: ", error);
+  }
+}
+
+async function getAllProductsByFilter(filter = {}) {
+  let sql = `
+    SELECT *
+    FROM PRODUCT P 
+    JOIN SUPPLIER S ON (S.SUPPLIER_ID = P.SUPPLIER_ID) 
+    JOIN CATEGORY C ON (C.CATEGORY_ID = P.CATEGORY_ID)
+    WHERE 1=1 
+  `;
+
+  const binds = {};
+
+  if (filter.category) {
+    sql += ` AND LOWER(REPLACE(C.CATEGORY_NAME, ' ', '')) LIKE LOWER(:categoryWithWildcards)`;
+    binds.categoryWithWildcards = `%${filter.category.toLowerCase()}%`;
+  }
+
+  if (filter.brand) {
+    sql += ` AND LOWER(P.BRAND) LIKE LOWER(:brandWithWildcards)`;
+    binds.brandWithWildcards = `%${filter.brand.toLowerCase()}%`;
+  }
+
+  if (filter.priceMin) {
+    sql += ` AND P.PRICE >= :priceMin`;  
+    binds.priceMin = filter.priceMin;
+  }
+
+  if (filter.priceMax) {
+    sql += ` AND P.PRICE <= :priceMax`;
+    binds.priceMax = filter.priceMax;
+  }
+
+  try {
+    const result = await database.dbexecute(sql, binds, database.dboptions);
+    if (result) {
+      return result.rows;
+    } else {
+      console.error('Query result is undefined.');
+      return [];
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+    return [];
+  }
+}
+
 module.exports = {
   getAllProduct,
   getAllCategories,
@@ -157,4 +300,8 @@ module.exports = {
   getProductBYId,
   getAllProductsBySupplier,
   searchProducts,
+  createProduct,
+  updateProductById,
+  getAllBrands,
+  getAllProductsByFilter,
 };
--- backend/router/Products/products.js
+++ backend/router/Products/products.js
@@ -1,3 +1,5 @@
+//http://localhost:5000/api/v1/products
+
 // libraries
 const express = require("express");
 const db_products = require("../../database/db-products");
@@ -35,6 +37,7 @@ router.get("/supplierId=:supplierId", async (req, res) => {
   res.json(productsOfSupplier);
 });
 
+
 router.get("/search", async(req, res) => {
   console.log(req.query)
   const  {q}  = req.query;
@@ -46,23 +49,19 @@ router.get("/search", async(req, res) => {
   console.log(searchResults);
   res.status(200).json(searchResults);
 });
- 
-router.get("/:id", async (req, res) => {
-  const product_by_id = await db_products.getProductBYId(req.params.id);
-  console.log("product_by_id");
-  res.json(product_by_id);
-});
 
 
 
 ///FUAD FUAD next er gula kor
 router.post("/create-product", async (req, res) => {
+  console.log(req.body);
   const create_product = await db_products.createProduct(req.body);/// database er product e lekhbi
   console.log("create_product");
   res.json(create_product);
 });
+
 router.patch("/update-productid=:id", async (req, res) => {
-  const update_product_by_id = await db_products.updateProductBYId(req.params.id);/// database er product e lekhbi
+  const update_product_by_id = await db_products.updateProductById(req.params.id, req.body);/// database er product e lekhbi
   console.log("update_product_by_id");
   res.json(update_product_by_id);
 });
@@ -72,58 +71,18 @@ router.get("/brands", async (req, res) => {
   console.log("all brands");
   res.json(all_brands);
 });
+router.get("/:id", async (req, res) => {
+  const product_by_id = await db_products.getProductBYId(req.params.id);
+  console.log("product_by_id");
+  res.json(product_by_id);
+});
 
 router.get("/", async (req, res) => {
-  const products_by_filter = await db_products.getAllProductsByFilter();/// database er product e lekhbi fetchall porducts er moto
+  const products_by_filter = await db_products.getAllProductsByFilter(req.body);/// database er product e lekhbi fetchall porducts er moto
   console.log("products_by_filter");
   res.json(products_by_filter);
 });
 
-// exports.fetchAllProducts = async (req, res) => {
-//   // filter = {"category":["smartphone","laptops"]}
-//   // sort = {_sort:"price",_order="desc"}
-//   // pagination = {_page:1,_limit=10}
-//   let condition = {}
-//   if(!req.query.admin){
-//       condition.deleted = {$ne:true}
-//   }
-  
-//   let query = Product.find(condition);
-//   let totalProductsQuery = Product.find(condition);
-
-//   console.log(req.query.category);
-
-//   if (req.query.category) {
-//     query = query.find({ category: {$in:req.query.category.split(',')} });
-//     totalProductsQuery = totalProductsQuery.find({
-//       category: {$in:req.query.category.split(',')},
-//     });
-//   }
-//   if (req.query.brand) {
-//     query = query.find({ brand: {$in:req.query.brand.split(',')} });
-//     totalProductsQuery = totalProductsQuery.find({ brand: {$in:req.query.brand.split(',') }});
-//   }
-//   if (req.query._sort && req.query._order) {
-//     query = query.sort({ [req.query._sort]: req.query._order });
-//   }
-
-//   const totalDocs = await totalProductsQuery.count().exec();
-//   console.log({ totalDocs });
-
-//   if (req.query._page && req.query._limit) {
-//     const pageSize = req.query._limit;
-//     const page = req.query._page;
-//     query = query.skip(pageSize * (page - 1)).limit(pageSize);
-//   }
-
-//   try {
-//     const docs = await query.exec();
-//     res.set('X-Total-Count', totalDocs);
-//     res.status(200).json(docs);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// };
 
 
 module.exports = router;
