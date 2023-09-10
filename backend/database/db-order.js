ca83650cd23bf00e82857108f416bcb476f404e3
MinhazFuad29 <minhajul.fuad@gmail.com>
2023-09-10 12:36:24 +0600

    Order updated

--- backend/database/db-order.js
+++ backend/database/db-order.js
@@ -2,14 +2,13 @@ const database = require("./create-and-executedb");
 
 async function getOrderBYCustomer(id) {
     const sql = `
-    SELECT O.ORDER_ID, O.ORDER_DATE, O.HOUSE, O.ROAD, O.AREA, O.CITY, O.STATE, O.CART_ID
-
-    SELECT C.CUSTOMER_ID, CA.CART_ID, PC.PRODUCT_ID, P.TITLLE, PC.QUANTITY, (PC.QUANTITY*P.PRICE) AS TOTAL_PRICE
-    FROM CUSTOMER C
-    JOIN CART CA ON (C.CUSTOMER_ID = CA.CUSTOMER_ID)
-    JOIN PRODUCT_CART PC ON (PC.CART_ID = CA.CART_ID)
+    SELECT O.ORDER_ID, O.ORDER_DATE, O.HOUSE, O.ROAD, O.AREA, O.CITY, O.STATE, O.CART_ID, S.SHIPPING_NAME, S.CHARGE, PC.PRODUCT_ID, P.TITLLE, PC.QUANTITY, PC.TOTAL_PRICE, P.IMAGE, P.BRAND
+    FROM ORDERS O 
+    JOIN SHIPPING_METHOD S ON (S.SHIPPING_ID = O.SHIPPING_ID)
+    JOIN PRODUCT_CART PC ON (PC.CART_ID = O.CART_ID)
     JOIN PRODUCT P ON (P.PRODUCT_ID = PC.PRODUCT_ID)
-    WHERE C.CUSTOMER_ID = :id
+    WHERE O.CUSTOMER_ID = :id
+    ORDER BY O.ORDER_DATE DESC
     `;
     const binds = {
       id: id
@@ -28,13 +27,122 @@ async function getOrderBYCustomer(id) {
     }
 }
 
+async function getOredrBySupplier(id) {
+  const sql = `
+  SELECT O.ORDER_ID, O.ORDER_DATE, O.HOUSE, O.ROAD, O.AREA, O.CITY, O.STATE, O.CART_ID, S.SHIPPING_NAME, S.CHARGE, PC.PRODUCT_ID, P.TITLLE, PC.QUANTITY, PC.TOTAL_PRICE, P.IMAGE, P.BRAND
+  FROM ORDERS O 
+  JOIN SHIPPING_METHOD S ON (S.SHIPPING_ID = O.SHIPPING_ID)
+  JOIN PRODUCT_CART PC ON (PC.CART_ID = O.CART_ID)
+  JOIN PRODUCT P ON (P.PRODUCT_ID = PC.PRODUCT_ID)
+  WHERE PC.SUPPLIER_ID = :id
+  ORDER BY O.ORDER_DATE DESC
+  `;
+  const binds = {
+    id: id
+  };
+  try {
+    const result = await database.dbexecute(sql, binds, database.dboptions);
+    if (result && result.rows) {
+      return result.rows;
+    } else {
+      console.error("Query result is undefined.");
+      return [];
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+    throw error;
+  }
+}
+
+async function getShippingMethodByOrder(id) {
+  const sql = `
+  SELECT S.SHIPPING_ID,
+  S.SHIPPING_NAME,
+  S.CHARGE 
+  FROM SHIPPING_METHOD
+  JOIN ORDERS O ON (O.SHIPPING_ID = S.SHIPPING_ID)
+  WHERE O.ORDER_ID = :id
+  `;
+  const binds = {
+    id: id,
+  };
+  console.log(binds);
+  try {
+    const result = await database.dbexecute(sql, binds, database.dboptions);
+    console.log(result);
+    if (result) {
+      return result.rows;
+    } else {
+      console.error("Query result is undefined.");
+      return null;
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+    throw error;
+  }
+}
+
+async function createOrder(orderData) {
+  const {ORDER_ID, ORDER_DATE, HOUSE, ROAD, AREA, CITY, STATE, CART_ID, CUSTOMER_ID, SHIPPING_ID} = orderData;
+  console.log(orderData);
+  const sql = `
+  INSERT INTO ORDERS (ORDER_ID, ORDER_DATE, HOUSE, ROAD, AREA, CITY, STATE, CART_ID, CUSTOMER_ID, SHIPPING_ID)
+  VALUES (:order_id, :order_date, :house, :road, :area, :city, :state, :cart_id, :customer_id, :shipping_id)
+  `;
+
+  const binds = {
+    order_id: ORDER_ID,
+    order_date: ORDER_DATE,
+    house: HOUSE,
+    road: ROAD,
+    area: AREA,
+    city: CITY,
+    state: STATE,
+    cart_id: CART_ID,
+    customer_id: CUSTOMER_ID,
+    shipping_id: SHIPPING_ID
+  };
+  try {
+    const result = await database.dbexecute(sql, binds, database.dboptions);
+    if (result) {
+      return result.rows[0];
+    } else {
+      console.error("Query result is undefined.");
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+}
+
+async function updateOrderTrackingById(id, updateShippingData) {
+  const sql =`
+  UPDATE SHIPPING_METHOD SM 
+  SET SM.STATUS = :shippinG_status,
+  WHERE SM.ORDER_ID = :id
+  `;
+
+  const binds = {
+    shipping_staus: updateShippingData.STATUS ,
+    id: id
+  };
+
+  try {
+    const result = await database.dbexecute(sql, binds, database.dboptions);
+    if (result) {
+      return "Shipping status updated successfully";
+    } else {
+      console.error('Query result is undefined.');
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+}
+
 
 module.exports = {
   getOrderBYCustomer,
-  updateOrderById, 
-  createOrder,  
-  deleteOrder,
+  getOredrBySupplier,
+  getShippingMethodByOrder,
+  createOrder, 
+  updateOrderTrackingById,
 };
-
-SUPPLIER ER ORDER DEKHA
-TRACKING ORDER UPDATE 
--- backend/router/Orders/Orders.js
+++ backend/router/Orders/Orders.js
@@ -7,46 +7,40 @@ const db_order = require("../../database/db-order.js");
 // creating router
 const router = express.Router({ mergeParams: true });
 
-router.get("/order=:id", async (req, res) => {
-    const order_by_user = await db_cart.getOrderBYCustomer(req.params.id);
+//Done
+router.get("/orders=:id", async (req, res) => {
+    const order_by_user = await db_order.getOrderBYCustomer(req.params.id);
     console.log("order_by_user");
     res.json(order_by_user);
 });
 
-router.patch("/update-cart=:id", async (req, res) => {
-    const update_cart_by_id = await db_cart.updateCartById(req.params.id, req.body);
-    console.log("update_customer_by_id");
-    res.json(update_cart_by_id);
+//Done
+router.get("/supplier-orders=:id", async (req, res) => {
+    const order_by_supplier = await db_order.getOredrBySupplier(req.paramsms.id);
+    console.log("order_by_supplier");
+    res.json(order_by_supplier);
 });
 
-router.post("/add", async (req, res) => {
-    console.log(req.body);
-    const add_to_cart = await db_cart.addToCart(req.body);
-    console.log("add_to_cart");
-    res.json(add_to_cart);
+router.patch("/update-order-tracking=:id", async (req, res) => {
+    const update_tracking_by_id = await db_order.updateOrderTrackingById(req.params.id, req.body);
+    console.log("update_tracking_by_id");
+    res.json(update_tracking_by_id);
 });
 
-router.delete("/delete/:id1/:id2", async(req, res) => {
-    const { id1, id2 } = req.params;
-    const deletedItem = await db_cart.deleteFromCart(id1, id2);
-    if(deletedItem) {
-        res.status(200).json({ message: 'Cart item deleted successfully' });
-    } else {
-        res.status(404).json({ error: 'Cart item not deleted' });
-    }
+//Done
+router.get('/shipping-method/:id', async (req,res) => {
+    const shipping_method_for_order = await db_order.getShippingMethodByOrder(req.params.id);
+    console.log(shipping_method_for_order);
+    res.json(shipping_method_for_order);
 });
 
-router.get('/shipping-method',async(req,res)=>{
-    console.log("yes")
-    res.send([{SHIPPING_ID:1,SHIPPING_NAME:"Courier",CHARGE:20},{SHIPPING_ID:2,SHIPPING_NAME:"Sundor Courier",CHARGE:30}])
-    console.log("yes2");
-});
+//Done
 router.post('/create-order',async(req,res)=>{
     console.log(req.body);
-    res.send();
-})
-
-
+    const create_order = await db_order.createOrder(req.body);
+    console.log("create_order");
+    res.json(create_order);
+});
 
 module.exports = router;
 
